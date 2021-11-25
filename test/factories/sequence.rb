08397d9bd6429b99c56915d613d78bc852ded019
vanushinvi <vanushinvi@gmail.com>
2021-11-25 13:36:35 +0400

    add validations

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -2,4 +2,7 @@ FactoryBot.define do
   sequence :string, aliases: [:first_name, :last_name, :password] do |n|
     "string#{n}"
   end
+  sequence :email do |n|
+    "stranger#{n}@example.xyz"
+  end
 end
