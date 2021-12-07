d2f851eccfef6a377bce050e902157cf4bc50fa6
vanushinvi <vanushinvi@gmail.com>
2021-12-07 13:21:57 +0400

    add userlist

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,8 @@ class Admin::UsersController < Admin::ApplicationController
     def show
       @user = User.find(params[:id])
     end
+    def index
+      @q = User.ransack(params[:q])
+      @users = @q.result.order(:type).order(:id).page(params[:page]).per(10)
+    end
   end
