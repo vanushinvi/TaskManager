dd46fe6d7fbd656d3742d6ec4fa80dbbc12f8b66
vanushinvi <vanushinvi@gmail.com>
2021-12-07 13:26:01 +0400

    add delete users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -30,6 +30,12 @@ class Admin::UsersController < Admin::ApplicationController
         render :edit
       end
     end
+
+    def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+      redirect_to admin_users_path
+    end
   
     private
   
