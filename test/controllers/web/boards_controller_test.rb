84905f2c8a9041e8e8afa3ee0c889103bda8fa70
vanushinvi <vanushinvi@gmail.com>
2021-12-07 15:55:57 +0400

    add menu

--- test/controllers/web/boards_controller_test.rb
+++ test/controllers/web/boards_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class Web::BoardsControllerTest < ActionController::TestCase
+  setup do
+    user = create(:user)
+    sign_in user
+  end
   test 'should get new' do
     get :show
     assert_response :success
