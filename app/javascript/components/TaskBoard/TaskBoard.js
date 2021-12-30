1d79d95293759d0277f0acc56b5c1b86897de93b
vanushinvi <vanushinvi@gmail.com>
2021-12-30 16:23:46 +0300

    add changing state

--- app/javascript/components/TaskBoard/TaskBoard.js
+++ app/javascript/components/TaskBoard/TaskBoard.js
@@ -60,6 +60,22 @@ const TaskBoard = function () {
     });
   };
 
+  const handleCardDragEnd = (task, source, destination) => {
+    const transition = task.transitions.find(({ to }) => destination.toColumnId === to);
+    if (!transition) {
+      return null;
+    }
+  
+    return TasksRepository.update(task.id, { stateEvent: transition.event })
+      .then(() => {
+        loadColumnInitial(destination.toColumnId);
+        loadColumnInitial(source.fromColumnId);
+      })
+      .catch((error) => {
+        alert(`Move failed! ${error.message}`);
+      });
+  };
+
   const generateBoard = () => {
     const board = {
       columns: STATES.map(({ key, value }) => ({
@@ -83,6 +99,7 @@ const TaskBoard = function () {
     <KanbanBoard
       renderCard={(card) => <Task task={card} />}
       renderColumnHeader={(column) => <ColumnHeader column={column} onLoadMore={loadColumnMore} />}
+      onCardDragEnd={handleCardDragEnd}
     >
       {board}
     </KanbanBoard>
