fb890829795b3e2d0982a4b8dc262b2cca612070
vanushinvi <vanushinvi@gmail.com>
2022-01-18 18:39:48 +0400

    Fix hooks

--- app/javascript/hooks/store/useTasks.js
+++ app/javascript/hooks/store/useTasks.js
@@ -1,15 +1,23 @@
 import { useSelector } from 'react-redux';
 import { useTasksActions } from 'slices/TasksSlice';
-import { states } from 'presenters/TaskPresenter';
+import { STATES } from 'presenters/TaskPresenter';
 
 const useTasks = () => {
-  const board = useSelector(state => state.TasksSlice.board);
-  const { loadColumn } = useTasksActions();
-  const loadBoard = () => Promise.all(states.map(({ key }) => loadColumn(key)));
+  const board = useSelector((state) => state.TasksSlice.board);
+  const { loadColumn, loadColumnMore, createTask, loadTask, destroyTask, updateTask, dragEndCard } = useTasksActions();
+
+  const loadBoard = () => Promise.all(STATES.map(({ key }) => loadColumn(key)));
 
   return {
     board,
     loadBoard,
+    loadColumn,
+    loadColumnMore,
+    createTask,
+    loadTask,
+    destroyTask,
+    updateTask,
+    dragEndCard,
   };
 };
 
